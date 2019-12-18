84d00f872bd5a94bbb32f7325265e0a853db6a57
Andara <andarasophan@gmail.com>
2019-12-18 16:38:48 +0700

    tambah folfer

---
+++ final-livecode-loyal/1.js
@@ -0,0 +1,34 @@
+/**
+ * ==========================
+ * What is my email provider?
+ * ==========================
+ *
+ * Description
+ * -----------
+ * Email merupakan sebuah cara untuk kita berinteraksi antar satu dengan yang lainnya secara elektronik,
+ * Banyak sekali provider yang menyediakan layanan email ini.
+ *
+ * Instruction
+ * -----------
+ * Buatlah sebuah fungsi yang akan mengeluarkan output provider email yang digunakan oleh user.
+ *
+ * Contoh
+ * =======
+ * @input => icha@hacktiv8.com
+ * @output => Your email provider is hacktiv8 ( tidak menggunakan .com di belakang)
+ *
+ * Rules
+ * =====
+ * 1. Wajib menggunakan PESUDOCODE / ALGORITMA atau -50
+ * 2. Tidak diperbolehkan menggunakan built-in function:
+ *    .map .filter .reduce .split .join .indexOf .findIndex
+ *
+ */
+
+function emailProvider(email) {
+  // Insert your code here
+}
+
+console.log(emailProvider('yusuf@gmail.com')) // Your email provider is gmail
+console.log(emailProvider('icha@hacktiv8.com')) // Your email provider is hacktiv8
+console.log(emailProvider('icha@yahoo.co.id')) // Your email provider is yahoo
---
+++ final-livecode-loyal/6.js
@@ -0,0 +1,26 @@
+/**
+ * ================
+ * String Evaluator
+ * ================
+ * 
+ * Instruction
+ * -----------
+ * Buatlah sebuah function untuk menghitung kalkulasi dari input yang bentuknya berupa string
+ * Operator yang digunakan adalah + - * /
+ * 
+ * Rules
+ * -----
+ * 1. WAJIB dilakukan dengan rekursif
+ * 2. dilarang menggunakan built-in function eval
+ * 3. Dilarang menambah dan merubah tipe data parameter
+ * 4. Dilarang membuat function lain selain evaluate
+ */
+
+function evaluate(str) {
+  // Write your code here
+}
+
+console.log(evaluate('1+2+3+4+5')) // 15
+console.log(evaluate('1*2*3*4*5')) // 120
+console.log(evaluate('20/4')) // 5
+console.log(evaluate('20-6')) // 14
